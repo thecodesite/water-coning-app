69523fffa870ca797f6e920784978e9197116106
Jose Perez <64388503+thecodesite@users.noreply.github.com>
2025-03-20 13:55:35 -0400

    Update calculations.py

--- src/utils/calculations.py
+++ src/utils/calculations.py
@@ -12,17 +12,20 @@ def schols(ko, h, hp, mu, rw, re, Denw, Deno, Bo):
 def muskat_wyckoff(ko, h, hp, mu, re, Denw, Deno, Bo, rw):
     return (0.0000924 * (((Denw - Deno) * ko) * (pow((1 - (pow((hp / h), 2))), 1.325)) / (mu * Bo)) * (pow(h, 2.238)) * pow((np.log(re / rw)), -1.99))
 
-def sobocinski_cornelius(kh, h, hp, mu, rw, re, Denw, Deno, Bo, Qo, kro, krw, mw, phi):
+def sobocinski_cornelius(kh, h, hp, mu, rw, re, Denw, Deno, Bo, Qo, kro, krw, mw, phi, kv):
+    # Dimensionless cone height Z
     Z = 0.0000492 * (Denw - Deno) * kh * h * (h - hp) / (mu * Bo * Qo)
+    # Dimensionless TBT
     TDbt = (4 * Z + pow(Z, 2) - 0.75 * pow(Z, 3)) / (7 - 2 * Z)
     M = (krw / kro) * (mu / mw)
     
     if M <= 1:
         alfa = 0.5
-    else:
+    elif M > 1:
         alfa = 0.6
     
     Tbt = (20325 * mu * h * phi * TDbt) / ((Denw - Deno) * kv * (1 + pow(M, alfa)))
+    
     Qoc_sobocinski_cornelius = 0.0000141 * ((Denw - Deno) * kh * h * (h - hp)) / (mu * Bo)
 
     return Tbt, Qoc_sobocinski_cornelius
